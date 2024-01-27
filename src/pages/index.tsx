c406382f98c506e9b3e34afc188471c94ddfa1b0
Blue Triangle <58819229+AoSankaku@users.noreply.github.com>
2024-01-28 03:42:10 +0900

    empty childImageSharp now returns undefined

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ const Home = ({
       const { id } = node
       const { slug } = node.fields!
       const { title, desc, date, category, thumbnail, alt } = node.frontmatter!
-      const { childImageSharp } = thumbnail ? thumbnail! : {}
+      const { childImageSharp } = thumbnail ? thumbnail! : { childImageSharp: undefined }
 
       setPosts(prevPost => [
         ...prevPost,
