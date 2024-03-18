9a0eca470e0140b2b5037d5def9fc9ad45bfa5ea
AoSankaku <58819229+AoSankaku@users.noreply.github.com>
2024-03-19 03:03:28 +0900

    Optimized share method for misskey and mastodon

--- src/components/ShareButtons.tsx
+++ src/components/ShareButtons.tsx
@@ -10,29 +10,33 @@ import {
 import MisskeyLogo from '../images/misskey_logo.png'
 import MastodonLogo from '../images/mastodon-icon-spacing.png'
 
+const meta = require("../../gatsby-meta-config")
+
 type Props = {
   title: string;
   articleUrl: string;
 }
 
 const ShareButtons: React.FC<Props> = ({ title, articleUrl }) => {
+  const shareText = `${title} | ${meta.title}`;
+  const encodedShareText = encodeURIComponent(shareText);
   return (
     <>
       <ShareText>シェアする？<wbr />（Misskey/Mastodon対応）</ShareText>
       <SharebuttonsWrapper>
-        <TwitterShareButton title={title} url={articleUrl}>
+        <TwitterShareButton title={shareText} url={articleUrl}>
           <TwitterIcon size={50} round />
         </TwitterShareButton>
-        <TwitterShareButton title={title} url={articleUrl}>
+        <TwitterShareButton title={shareText} url={articleUrl}>
           <XIcon size={50} round />
         </TwitterShareButton>
-        <a href={`https://misskey-hub.net/share/?text=${title}&url=${articleUrl}&visibility=public&localOnly=0`} target="_blank" rel="noopener">
+        <a href={`https://misskey-hub.net/share/?text=${encodedShareText}&url=${articleUrl}&visibility=public&localOnly=0`} target="_blank" rel="noopener">
           <img src={MisskeyLogo} alt="Misskey Logo" style={{ width: "50px", backgroundColor: "white", borderRadius: "25px" }} />
         </a>
-        <a href={`https://donshare.net/share.html?text=${title}&url=${articleUrl}`} target="_blank" rel="noopener">
+        <a href={`https://donshare.net/share.html?text=${encodedShareText}&url=${articleUrl}`} target="_blank" rel="noopener">
           <img src={MastodonLogo} alt="Mastodon Logo" style={{ width: "50px", backgroundColor: "#858AFA", borderRadius: "25px", objectFit: "fill" }} />
         </a>
-        <LineShareButton title={title} url={articleUrl}>
+        <LineShareButton title={shareText} url={articleUrl}>
           <LineIcon size={50} round />
         </LineShareButton>
       </SharebuttonsWrapper>
